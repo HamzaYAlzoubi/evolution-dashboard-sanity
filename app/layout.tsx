9f240eb6ae9d4bc155b206d1fd8172829cd2227a
hamza <hamzaalzoubi94@gmail.com>
2025-08-16 16:10:46 -0700

    v8 fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ar" dir="rtl" className="dark">
+    <html lang="ar" dir="rtl">
       <body className="bg-gray-50 dark:bg-gray-900 dark:text-gray-100">
         {children}
       </body>
