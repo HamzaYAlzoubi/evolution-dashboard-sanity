90b9318ef9baf25b8aebab57843f363dca98fb5d
Hamza Y Alzoubi <hamzaalzoubi94@gmail.com>
2025-08-26 18:00:25 -0700

    v13 fix 6

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -2,8 +2,8 @@ import HomeSessionsForm from "@/components/home/HomeSessionsForm";
 
 export default function Home() {
   return (
-    <div className="sm:pt-0 pt-5 bg-white">
+    <>
       <HomeSessionsForm />
-    </div>
+    </>
   );
 }
